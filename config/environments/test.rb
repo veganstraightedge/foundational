b7296a239bab525862a92dc68764fe13acac33d3
Shane Becker <veganstraightedge@gmail.com>
2020-06-26 02:26:07 -0700

    Add action_mailer.default_url_options host config
    For Devise

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # For Devise
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
