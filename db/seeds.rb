388a333fdea3132f440c7b2b6d733e3fc8778f43
Shane Becker <veganstraightedge@gmail.com>
2020-06-27 00:24:18 -0700

    Add db seeds with admin user and Settings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts '==> Seeding…'
+
+if Rails.env.development?
+  puts '==> Creating default admin user…'
+  admin_user = User.create! email:                 'admin@example.com',
+                            password:              '123456789012345678901234567890',
+                            password_confirmation: '123456789012345678901234567890'
+
+  # Confirm admin user email
+  admin_user.update confirmed_at: Time.current, remember_created_at: Time.current
+end
+
+# Settings
+puts '==> Creating required Settings with default content…'
+Setting.create! name:         'Host name',
+                description:  'What domain will you use for your website? For example: example.com.',
+                content:      ENV.fetch('WHEN_AND_WHERE_HOST') { 'example.com' },
+                form_control: 'text_field'
+
+Setting.create! name:         'Default locale',
+                description:  'What locale (language + optional dialect / region) your website be used in primarily?
+                              For example: “en” for English (no dialect), “pt-br” for Brazilian Portuguese,
+                              “it” for Italian, etc.'.squish,
+                content:      ENV.fetch('WHEN_AND_WHERE_LOCALE') { 'en' },
+                form_control: 'text_field'
+
+Setting.create! name:         'FROM email address',
+                description:  'What address will your site send emails FROM? For example, the password reset email.',
+                content:      ENV.fetch('WHEN_AND_WHERE_FROM_EMAIL') { 'from@example.com' },
+                form_control: 'email_field'
+
+Setting.create! name:         'Minimum password length',
+                description:  'How long does a user’s password have to be?',
+                content:      ENV.fetch('WHEN_AND_WHERE_PASSWORD_MINIMUM') { 12 },
+                form_control: 'number_field'
+
+puts '==> Seeding… complete.'
