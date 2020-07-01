1f1f182bcf643b3354c0914922d0835b8aaafbb7
Shane Becker <veganstraightedge@gmail.com>
2020-07-01 01:28:13 -0700

    Add Tag validation and callbacks

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,19 @@
 class Tag < ApplicationRecord
+  has_many :taggings, dependent: :destroy
+
+  before_validation :remove_extra_spaces, on: %i[create update]
+  before_validation :generate_slug,       on: %i[create update]
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :slug, uniqueness: true
+
+  private
+
+  def remove_extra_spaces
+    self.name = name.to_s.squish
+  end
+
+  def generate_slug
+    self.slug = name.to_s.transliterate.parameterize
+  end
 end
