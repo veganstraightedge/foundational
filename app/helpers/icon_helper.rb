50077c75eead4cd1e63db9bd26e5c7fc1602606a
Shane Becker <veganstraightedge@gmail.com>
2020-07-18 00:28:45 -0700

    Add gear icon to Site Settings heading

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -8,6 +8,6 @@ module IconHelper
   end
 
   def icon_tag name, options = {}
-    Icon.for :map, size: "30x30", class: "d-inline-block align-top mr-1", loading: "lazy"
+    Icon.for name, options
   end
 end
