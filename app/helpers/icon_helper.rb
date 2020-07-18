115979df5747d308ceac49c7bd55f20d7a8a84ec
Shane Becker <veganstraightedge@gmail.com>
2020-07-18 00:23:49 -0700

    Extract icon helper methods to Icon Helper

---
+++ app/helpers/icon_helper.rb
@@ -0,0 +1,13 @@
+module IconHelper
+  def image_tag_for_brand
+    if setting_for_brand_image.present?
+      tag.image setting_for_brand_image
+    else
+      icon_tag :map, size: "30x30", class: "d-inline-block align-top mr-1", loading: "lazy"
+    end
+  end
+
+  def icon_tag name, options = {}
+    Icon.for :map, size: "30x30", class: "d-inline-block align-top mr-1", loading: "lazy"
+  end
+end
