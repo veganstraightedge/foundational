267e516d292b69eeb835fd9833f073a5baa37ce5
Shane Becker <veganstraightedge@gmail.com>
2020-07-19 01:16:43 -0700

    Validate username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ApplicationRecord
   rolify
   devise :confirmable, :database_authenticatable, :invitable, :lockable,
          :recoverable, :registerable, :rememberable, :timeoutable, :validatable
+
+         before_validation :sanitize_username
+
+  validates :username,
+            format: { with: /\A[\w\d]+\z/ },
+            length: { minimum: 1, maximum: 50, },
+            presence: true,
+            uniqueness: true,
+            allow_blank: true
+
+  private
+
+  def sanitize_username
+    self.username.strip!
+  end
 end
