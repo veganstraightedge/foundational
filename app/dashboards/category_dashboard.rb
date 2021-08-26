3c793bcccfda7ae53aa0a91f399d4c30c5c47497
Shane Becker <veganstraightedge@gmail.com>
2021-08-25 21:47:30 -0700

    Layout/FirstArrayElementIndentation

--- app/dashboards/category_dashboard.rb
+++ app/dashboards/category_dashboard.rb
@@ -22,7 +22,7 @@ class CategoryDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  categorizations
+    categorizations
   id
   name
   slug
@@ -31,7 +31,7 @@ class CategoryDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  categorizations
+    categorizations
   id
   name
   slug
@@ -43,7 +43,7 @@ class CategoryDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  categorizations
+    categorizations
   name
   slug
   ].freeze
--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -31,7 +31,7 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  id
+    id
   email
   encrypted_password
   reset_password_token
@@ -40,7 +40,7 @@ class UserDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  id
+    id
   email
   encrypted_password
   reset_password_token
@@ -61,7 +61,7 @@ class UserDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  email
+    email
   encrypted_password
   reset_password_token
   reset_password_sent_at
