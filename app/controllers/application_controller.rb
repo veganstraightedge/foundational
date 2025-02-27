79a03a8e216be7e3a9ea191fabb654bdd68b2ba2
Shane Becker <veganstraightedge@gmail.com>
2025-02-27 13:23:31 -0800

    App controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: %i[username]
+  end
+
+  private
+
+  def set_current_user
+    Current.user = current_user
+  end
 end
