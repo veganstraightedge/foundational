fe4b32b526af8f6c7764b632df51513cd5b62e1c
Shane Becker <veganstraightedge@gmail.com>
2025-02-27 15:04:59 -0800

    Active Storage migration from Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2021_08_25_073957) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_27_230356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,7 +30,7 @@ ActiveRecord::Schema[8.0].define(version: 2021_08_25_073957) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
