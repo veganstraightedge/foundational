37f02b77dea2beb4068f65b2c3550ca2e588a8ca
Shane Becker <veganstraightedge@gmail.com>
2025-02-27 22:56:35 -0800

    Get Bootstrap JS via importmaps working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// Configure your import map in config/importmap.rb.
+// Read more: https://github.com/rails/importmap-rails
+import '@hotwired/turbo-rails'
+import 'controllers'
+
+// bootstrap + popper
+// download from: https://cdn.jsdelivr.net/npm/bootstrap@latest/+esm
+// in:   /vendor/javascript/bootstrap.bundle.js
+import 'bootstrap.bundle.js'
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,13 @@
 # Pin npm packages by running ./bin/importmap
-
 pin 'application'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js'
-pin '@hotwired/stimulus', to: 'stimulus.min.js'
+
+# hotwire.js
+pin '@hotwired/turbo-rails',      to: 'turbo.min.js'
+pin '@hotwired/stimulus',         to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+
+# stimulus controllers
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+
+# bootstrap + popper js
+pin 'bootstrap.bundle.js', to: 'https://cdn.jsdelivr.net/npm/bootstrap@latest/+esm', preload: true
