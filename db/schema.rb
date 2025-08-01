2f589ea58fd5f2830aef39a6e2641b37d59be5a8
Shane Becker <veganstraightedge@gmail.com>
2025-08-01 16:25:37 -0700

    Rails/UniqueValidationWithoutIndex

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_01_231051) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_01_232422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_01_231051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
   create_table "categorizations", force: :cascade do |t|
